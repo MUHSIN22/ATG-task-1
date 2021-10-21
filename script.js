bc56a35e30606b86365d0549c3f88729b45ff638
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-10-21 16:14:57 +0530

    style link changed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let signUp = document.querySelector(".sign-up-wrapper")
 let signIn = document.querySelector(".sign-in-wrapper")
 let passwordVisible = [false,false]
+let isOptions = false
 
 const showSignUp = () =>{
     signIn.style.display = "none"
@@ -28,14 +29,36 @@ const togglePasswordVisibility = (index) => {
 }
 
 const setLogin = (event) => {
-    console.log(event);
-    event.preventDefault();
+    event && event.preventDefault();
     signUp.style.display = "none";
     signIn.style.display = "none";
     
     let createAccount = document.getElementById('btn-create-account')
     let headerProfile = document.querySelector('.profile-on-header')
+    let groupSection = document.querySelector('.recommended-groups-wrapper')
+    groupSection.style.display = "flex"
     createAccount.style.display = "none";
     headerProfile.style.display = "block"
     
+}
+
+const navLinkClick = (event) => {
+    let enabled = document.querySelector('.enabled-nav-link')
+    enabled.classList.remove("enabled-nav-link")
+    event.target.classList.add("enabled-nav-link")
+}
+
+const showOptions = (event) =>{
+    if(event.target.nextElementSibling){
+        event.target.nextElementSibling.style.display = isOptions?"none":"block";
+        isOptions = !isOptions
+    }else{
+        event.target.children[1].style.display = isOptions?"none":"block";
+        isOptions = !isOptions
+    }
+}
+
+const optionSelected = (event) => {
+    let options = document.querySelector('.option-list')
+    options.style.display = "none"
 }
