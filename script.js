48389a5db7f90e386b9b545585c3844946ac1a30
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-10-21 11:59:55 +0530

    login and sign up pages are done

---
+++ script.js
@@ -0,0 +1,8 @@
+let signUp = document.querySelector(".sign-up-wrapper")
+
+const showSignUp = () =>{
+    signUp.style.display = "grid";
+}
+const closeSignUp = () =>{
+    signUp.style.display = "none";
+}
